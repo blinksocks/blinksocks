18d91eb0ef80e0565a671c5a51ded3cfd902f2d1
Micooz <micooz@hotmail.com>
2017-01-01 12:04:16 +0800

    chore(deploy): display config file to stdout

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 CONFIG_FILE=/blinksocks/server.config.json
 RANDOM_PASSWORD=`pwgen -cyn -1 16`
 
-echo ">>> Generated random password: ${RANDOM_PASSWORD}"
+echo ">>> Generated random password..."
 sed -i -e"s/^\"password\".*:.*/\"password\": \"${RANDOM_PASSWORD}\"/" ${CONFIG_FILE}
+
+echo ">>> Using ${CONFIG_FILE}..."
+cat ${CONFIG_FILE}
 
 echo ">>> Running blinksocks with ${CONFIG_FILE}"
 blinksocks -c ${CONFIG_FILE}
