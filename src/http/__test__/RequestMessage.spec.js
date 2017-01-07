e0d638e6d2adfa9d83eed1186cbf788d0bc8823d
Micooz <micooz@hotmail.com>
2017-01-07 13:56:25 +0800

    fix(test): add spec to test invalid http message

--- src/http/__test__/RequestMessage.spec.js
+++ src/http/__test__/RequestMessage.spec.js
@@ -13,6 +13,11 @@ Host: www.bing.com:443\r\n
     expect(RequestMessage.parse(requestData)).toBe(null);
   });
 
+  it('should return null if buffer is not a valid http message', function () {
+    const requestData = Buffer.from('XXX www.bing.com:443 HTTP/1.1');
+    expect(RequestMessage.parse(requestData)).toBe(null);
+  });
+
   it('should return an instance', function () {
     const requestData = Buffer.from(`CONNECT www.bing.com:443 HTTP/1.1\r\n
 Host: www.bing.com:443\r\n
