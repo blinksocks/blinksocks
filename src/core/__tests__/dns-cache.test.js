a8152640c0ac1c6f76627cc1f058a307a720c837
Micooz <micooz@hotmail.com>
2017-02-23 21:42:24 +0800

    test(core): add tests for address, config and dns-cache

---
+++ src/core/__tests__/dns-cache.test.js
@@ -0,0 +1,43 @@
+import net from 'net';
+import {DNSCache, DNS_SURVIVAL_TIME} from '../dns-cache';
+
+describe('DNSCache#create', function () {
+
+  it('should return an instance of DNSCache', function () {
+    expect(DNSCache.create()).toBeInstanceOf(DNSCache);
+  });
+
+});
+
+describe('DNSCache#get', function () {
+
+  it('should return an ip address', async function () {
+    const dns = DNSCache.create();
+    expect(net.isIP(await dns.get('localhost'))).toBe(4);
+    expect(net.isIP(await dns.get('localhost'))).toBe(4);
+  });
+
+  it('should return null if fail to resolve', async function () {
+    const dns = DNSCache.create();
+    expect(await dns.get('xxx')).toBe(null);
+  });
+
+  it('should remove from this._poll if expire', async function () {
+    const dns = DNSCache.create();
+    await dns.get('localhost');
+    dns._now = () => (new Date()).getTime() + DNS_SURVIVAL_TIME;
+    await dns.get('localhost');
+    expect(dns._pool).toEqual({});
+  });
+
+});
+
+describe('DNSCache#_now', function () {
+
+  it('should return a timestamp', function () {
+    const dns = DNSCache.create();
+    const now = (new Date()).getTime();
+    expect(dns._now()).toBeGreaterThanOrEqual(now);
+  });
+
+});
