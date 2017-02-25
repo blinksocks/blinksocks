9c75a9214f628bd7c2c63a10e0c76d841d90b795
Micooz <micooz@hotmail.com>
2017-02-25 21:49:44 +0800

    refactor(bootstrap): support new interfaces

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -2,10 +2,42 @@ const fs = require('fs');
 const program = require('commander');
 const packageJson = require('../package.json');
 
+const options = [
+  ['-c, --config [file]', 'a json file for configuration, if specified, ignore other options', ''],
+  ['--host <host>', 'an ip address or a hostname to bind'],
+  ['--port <port>', 'where to listen on'],
+  ['--server-host [server-host]', 'an ip address or a hostname to connect'],
+  ['--server-port [server-port]', 'where is the server listen on'],
+  ['--key <key>', 'a key for encryption and decryption'],
+  ['--crypto [crypto]', 'a preset used in crypto middleware', 'none'],
+  ['--crypto-params [crypto-params]', 'parameters for crypto preset', ''],
+  ['--protocol [protocol]', 'a preset used in protocol middleware', 'none'],
+  ['--protocol-params [protocol-params]', 'parameters for protocol preset', ''],
+  ['--obfs [obfs]', 'a preset used in obfs middleware', 'none'],
+  ['--obfs-params [obfs-params]', 'parameters for obfs preset', ''],
+  ['--log-level [log-level]', 'log4js log level', 'all'],
+  ['-q, --quiet', 'limit log level to \'error\''],
+  ['--ciphers', 'show all supported ciphers'],
+  ['--hashes', 'show all supported hash functions']
+];
+
+const examples = `
+  Examples:
+  
+  As simple as possible:
+    $ blinksocks -c config.json
+  
+  To start a server:
+    $ blinksocks --host 0.0.0.0 --port 7777 --key key --crypto openssl --crypto-params aes-256-cfb
+  
+  To start a client:
+    $ blinksocks --host localhost --port 1080 --server-host example.com --server-port 7777 --key key --crypto openssl --crypto-params aes-256-cfb
+`;
+
 /**
  * get raw config object from json or command line options
  * @param options
- * @returns {*}
+ * @returns {object}
  */
 function obtainConfig(options) {
   if (options.config !== '') {
@@ -21,50 +53,39 @@ function obtainConfig(options) {
     return config;
   }
   const {host, port, serverHost, serverPort} = options;
-  const {password, cipher, logLevel} = options;
-  const {useIv, quiet} = options;
-  return {
+  const {key, crypto, cryptoParams, protocol, protocolParams, obfs, obfsParams, logLevel} = options;
+  const {quiet} = options;
+  const config = {
     host,
     port: parseInt(port, 10),
-    server_host: serverHost,
-    server_port: parseInt(serverPort, 10),
-    password,
-    cipher,
-    'use_iv': typeof useIv === 'undefined' ? false : useIv,
-    'log_level': typeof quiet === 'undefined' ? logLevel : 'error'
+    key,
+    crypto,
+    crypto_params: cryptoParams,
+    protocol,
+    protocol_params: protocolParams,
+    obfs,
+    obfs_params: obfsParams,
+    log_level: typeof quiet === 'undefined' ? logLevel : 'error'
   };
+  if (serverHost) {
+    Object.assign(config, {
+      server_host: serverHost,
+      server_port: parseInt(serverPort, 10)
+    });
+  }
+  return config;
 }
 
 module.exports = function ({Hub, Crypto}) {
-  program
+  const pg = program
     .version(packageJson.version)
-    .usage('[options] [...]')
-    .option('-c, --config [file]', 'a json file for configuration, if specified, ignore other options', '')
-    .option('--host <host>', 'an ip address or a hostname to bind')
-    .option('--port <port>', 'where to listen on')
-    .option('--password <password>', 'a password for encryption and decryption')
-    .option('--server-host [serverHost]', 'an ip address or a hostname to connect')
-    .option('--server-port [serverPort]', 'where is the server listen on')
-    .option('--cipher [cipher]', 'a method for encryption or decryption, leave it empty to enbaled non-encryption mode', '')
-    .option('--use-iv', 'if use initialization vector for encryption')
-    .option('--log-level [logLevel]', 'log4js log level', 'all')
-    .option('-q, --quiet', 'limit log level to \'error\'')
-    .option('--ciphers', 'show all supported ciphers');
+    .usage('--host <host> --port <port> --key <key> [...]');
 
-  program.on('--help', function () {
-    console.log('  Examples:');
-    console.log('');
-    console.log('  As simple as possible:');
-    console.log('    $ blinksocks -c config.json');
-    console.log('');
-    console.log('  To start a server:');
-    console.log('    $ blinksocks --host 0.0.0.0 --port 7777 --password password --cipher \"aes-256-cfb\"');
-    console.log('');
-    console.log('  To start a client:');
-    console.log('    $ blinksocks --host localhost --port 1080 --password password' +
-      ' --server-host example.com --server-port 7777 --cipher \"aes-256-cfb\"');
-  });
+  for (const option of options) {
+    pg.option(...option);
+  }
 
+  program.on('--help', () => console.log(examples));
   program.parse(process.argv);
 
   // no options provided
@@ -75,7 +96,13 @@ module.exports = function ({Hub, Crypto}) {
 
   // --ciphers
   if (program.ciphers) {
-    console.log(Crypto.getCiphers().join('\n'));
+    console.log(Crypto.getAvailableCiphers().join('\n'));
+    process.exit(0);
+  }
+
+  // --hashes
+  if (program.hashes) {
+    console.log(Crypto.getAvailableHashes().join('\n'));
     process.exit(0);
   }
 
@@ -84,16 +111,7 @@ module.exports = function ({Hub, Crypto}) {
     try {
       const app = new Hub(config);
       app.run();
-
-      // let quitCount = 2;
-      process.on('SIGINT', () => {
-        // if (--quitCount > 0) {
-        // console.log('Gracefully shutting down from SIGINT (Ctrl+C)');
-        // process.exit(0);
-        // } else {
-        process.exit(0);
-        // }
-      });
+      process.on('SIGINT', () => process.exit(0));
     } catch (err) {
       console.error(err.message);
       process.exit(-1);
