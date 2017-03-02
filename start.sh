a401f848eadb59cf86d953c103dd328b618b3733
Micooz <micooz@hotmail.com>
2017-03-02 14:00:00 +0800

    fix(deploy): update start.sh to use new configuration

--- start.sh
+++ start.sh
@@ -3,21 +3,33 @@
 CONFIG_FILE=/blinksocks/server.config.json
 BIND_HOST="0.0.0.0"
 BIND_PORT=1080
-RANDOM_PASSWORD=`pwgen --capitalize --numerals -1 16`
-DEFAULT_CIPHER="aes-256-cfb"
-USE_IV="true"
+RANDOM_KEY=`pwgen --capitalize --numerals -1 16`
+FRAME="origin"
+FRAME_PARAMS=""
+CRYPTO="openssl"
+CRYPTO_PARAMS="aes-256-cbc"
+PROTOCOL="aead"
+PROTOCOL_PARAMS="aes-128-cbc,sha256"
+OBFS=""
+OBFS_PARAMS=""
 LOG_LEVEL="error"
 
 mkdir -p /blinksocks && touch ${CONFIG_FILE}
 
-echo ">>> Generated random password..."
+echo ">>> Generated configuration with random key..."
 cat <<-EOF > ${CONFIG_FILE}
 {
   "host": "${BIND_HOST}",
   "port": ${BIND_PORT},
-  "password": "${RANDOM_PASSWORD}",
-  "cipher": "${DEFAULT_CIPHER}",
-  "use_iv": ${USE_IV},
+  "key": "${RANDOM_KEY}",
+  "frame": "${FRAME}",
+  "frame_params": "${FRAME_PARAMS}",
+  "crypto": "${CRYPTO}",
+  "crypto_params": "${CRYPTO_PARAMS}",
+  "protocol": "${PROTOCOL}",
+  "protocol_params": "${PROTOCOL_PARAMS}",
+  "obfs": "${OBFS}",
+  "obfs_params": "${OBFS_PARAMS}",
   "log_level": "${LOG_LEVEL}"
 }
 EOF
