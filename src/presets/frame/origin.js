e91b83e851e3ba92c57dfd7b1937cf6f48a78e9f
Micooz <micooz@hotmail.com>
2017-03-05 21:48:58 +0800

    refactor(presets/frame): deprecate pack() and unpack()

--- src/presets/frame/origin.js
+++ src/presets/frame/origin.js
@@ -1,11 +1,13 @@
+import net from 'net';
+import ip from 'ip';
 import logger from 'winston';
 import {IPreset} from '../interface';
 import {SOCKET_CONNECT_TO_DST} from '../actions';
-import {Address} from '../../core/address';
+import {Utils} from '../../utils';
 
 import {
   ATYP_V4,
-  // ATYP_V6,
+  ATYP_V6,
   ATYP_DOMAIN
 } from '../../proxies/common';
 
@@ -35,22 +37,42 @@ import {
  *   +----------+
  *   | Variable |
  *   +----------+
+ *
+ * @explain
+ *   1. ATYP is one of [0x01(ipv4), 0x03(hostname), 0x04(ipv6)].
+ *   2. When ATYP is 0x03, DST.ADDR[0] is len(DST.ADDR).
+ *   3. When ATYP is 0x04, DST.ADDR is a 16 bytes ipv6 address.
  */
 export default class OriginFrame extends IPreset {
 
-  _targetAddress = null; // client use only
-
   _isHandshakeDone = false;
 
-  constructor(address) {
+  _atyp = ATYP_V4;
+
+  _addr = null; // buffer
+
+  _port = null; // buffer
+
+  constructor(addr) {
     super();
-    this._targetAddress = address;
+    if (__IS_CLIENT__) {
+      const {type, host, port} = addr;
+      this._atyp = type;
+      this._addr = net.isIP(host) ? ip.toBuffer(host) : Buffer.from(host);
+      this._port = port instanceof Buffer ? port : Utils.numberToUIntBE(port);
+    }
   }
 
   clientOut({buffer}) {
     if (!this._isHandshakeDone) {
       this._isHandshakeDone = true;
-      return this.pack(this._targetAddress, buffer);
+      return Buffer.from([
+        this._atyp,
+        ...(this._atyp === ATYP_DOMAIN) ? Utils.numberToUIntBE(this._addr.length, 1) : [],
+        ...this._addr,
+        ...this._port,
+        ...buffer
+      ]);
     } else {
       return buffer;
     }
@@ -58,21 +80,61 @@ export default class OriginFrame extends IPreset {
 
   serverIn({buffer, next, broadcast}) {
     if (!this._isHandshakeDone) {
-      const frame = this.unpack(buffer);
-      if (frame === null) {
-        throw Error(`dropped unidentified packet ${buffer.length} bytes: ${buffer.toString('hex').substr(0, 60)}`);
+      if (buffer.length < 7) {
+        logger.error(`invalid length: ${buffer.length}`);
+        return;
       }
-      const {ATYP, DSTADDR, DSTPORT, DATA} = frame;
+
+      const atyp = buffer[0];
+      if (![ATYP_DOMAIN, ATYP_V4, ATYP_V6].includes(atyp)) {
+        logger.error(`invalid atyp: ${atyp}`);
+        return;
+      }
+
+      let addr, port; // string
+      let offset = 3;
+
+      switch (atyp) {
+        case ATYP_V4:
+          addr = ip.toString(buffer.slice(1, 5));
+          port = buffer.slice(5, 7).readUInt16BE(0);
+          offset += 4;
+          break;
+        case ATYP_V6:
+          if (buffer.length < 19) {
+            logger.error(`invalid length: ${buffer.length}`);
+            return;
+          }
+          addr = ip.toString(buffer.slice(1, 16));
+          port = buffer.slice(16, 18).readUInt16BE(0);
+          offset += 16;
+          break;
+        case ATYP_DOMAIN:
+          const domainLen = buffer[1];
+          if (buffer.length < domainLen + 4) {
+            logger.error(`invalid length: ${buffer.length}`);
+            return;
+          }
+          addr = buffer.slice(2, 2 + domainLen).toString();
+          port = buffer.slice(2 + domainLen, 4 + domainLen).readUInt16BE(0);
+          offset += domainLen + 1;
+          break;
+        default:
+          logger.error(`invalid atyp: ${atyp}`);
+          return;
+      }
+
       // notify to connect to the real server
       broadcast({
         type: SOCKET_CONNECT_TO_DST,
-        payload: [
-          new Address({ATYP, DSTADDR, DSTPORT}),
-          () => { // once connected
-            next(DATA);
-            this._isHandshakeDone = true;
-          }
-        ]
+        payload: [{
+          type: atyp,
+          host: addr,
+          port
+        }, () => { // once connected
+          next(buffer.slice(offset));
+          this._isHandshakeDone = true;
+        }]
       });
     } else {
       return buffer;
@@ -85,70 +147,6 @@ export default class OriginFrame extends IPreset {
 
   clientIn({buffer}) {
     return buffer;
-  }
-
-  pack(address, data) {
-    const {ATYP, DSTADDR, DSTPORT} = address;
-
-    let addr = null;
-    if (ATYP === ATYP_DOMAIN) {
-      addr = [DSTADDR.length, ...DSTADDR];
-    } else {
-      addr = DSTADDR;
-    }
-
-    return Buffer.from([ATYP, ...addr, ...DSTPORT, ...data]);
-  }
-
-  unpack(buffer) {
-    const _buffer = Buffer.from(buffer);
-
-    if (_buffer.length < 7) {
-      logger.debug(`invalid length: ${_buffer.length}`);
-      return null;
-    }
-
-    let DSTADDR = null;
-    let DSTPORT = null;
-    switch (_buffer[0]) {
-      case ATYP_V4: {
-        DSTADDR = _buffer.slice(1, 5);
-        DSTPORT = _buffer.slice(5, 7);
-        break;
-      }
-      // case ATYP_V6: {
-      //   if (_buffer.length < 21) {
-      //     logger.debug(`invalid length: ${_buffer.length}`);
-      //     return null;
-      //   }
-      //   DSTADDR = _buffer.slice(2, 19);
-      //   DSTPORT = _buffer.slice(19, 21);
-      //   break;
-      // }
-      case ATYP_DOMAIN: {
-        const domainLen = _buffer[1];
-        if (_buffer.length < 4 + domainLen) {
-          logger.debug(`invalid length: ${_buffer.length}`);
-          return null;
-        }
-        DSTADDR = _buffer.slice(2, 2 + domainLen);
-        DSTPORT = _buffer.slice(2 + domainLen, 4 + domainLen);
-        break;
-      }
-      default:
-        logger.debug(`unknown ATYP: ${_buffer[0]}`);
-        return null;
-    }
-
-    return {
-      ATYP: _buffer[0],
-      DSTADDR,
-      DSTPORT,
-      DATA: _buffer.slice(DSTADDR.length + (_buffer[0] === ATYP_DOMAIN ? 4 : 3)),
-      toBuffer() {
-        return Buffer.from([this.ATYP, ...this.DSTADDR, ...this.DSTPORT, ...this.DATA]);
-      }
-    };
   }
 
 }
