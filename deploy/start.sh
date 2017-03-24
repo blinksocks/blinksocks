741b37ebb2588d049c4ba2f1ae2e29ca383e81a2
Micooz <micooz@hotmail.com>
2017-03-24 17:08:10 +0800

    refactor(deploy): use "blinksocks init" to generate config.json

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-RANDOM_KEY=`pwgen --capitalize --numerals -1 16`
-
-cd /blinksocks
-
-echo ">>> Generating random key..."
-sed -i -e"s/\"key\": \".*\"/\"key\": \"${RANDOM_KEY}\"/"  blinksocks.config.json
-
-pm2 start pm2.config.json
+cd /blinksocks && blinksocks init && pm2 start pm2.config.json
