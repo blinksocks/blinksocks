a981f835f45c9887c6fa2ae5882ef5621ecb4633
Micooz <micooz@hotmail.com>
2017-08-10 18:04:32 +0800

    presets: [unfinished] proxy.js

--- src/presets/proxy.js
+++ src/presets/proxy.js
@@ -1,6 +1,10 @@
 import ip from 'ip';
 import {IPreset, SOCKET_CONNECT_TO_DST, PROXY_HANDSHAKE_DONE} from './defs';
 import {Proxifier, ATYP_DOMAIN} from '../proxies';
+import {isValidHostname, isValidPort} from '../utils';
+
+const MODE_PROXY = 0;
+const MODE_TUNNEL = 1;
 
 /**
  * @description
@@ -11,10 +15,10 @@ import {Proxifier, ATYP_DOMAIN} from '../proxies';
  *   port(optional): destination port
  *
  * @examples
- *   proxy mode, using proxifier
+ *   - proxy mode, using proxifier
  *   {"name": "proxy"}
  *
- *   tunnel mode, relay directly
+ *   - tunnel mode, relay directly
  *   {
  *     "name": "proxy",
  *     "params": {
@@ -26,6 +30,29 @@ import {Proxifier, ATYP_DOMAIN} from '../proxies';
 export default class ProxyPreset extends IPreset {
 
   _proxy = null;
+
+  _mode = MODE_PROXY;
+
+  _host = null;
+
+  _port = null;
+
+  _isTunnelReady = false;
+
+  constructor({host, port}) {
+    super();
+    if (typeof host !== 'undefined' && !isValidHostname(host)) {
+      throw Error('host is invalid');
+    }
+    if (typeof port !== 'undefined' && !isValidPort(port)) {
+      throw Error('port is invalid');
+    }
+    if (host && port) {
+      this._mode = MODE_TUNNEL;
+      this._host = host;
+      this._port = port;
+    }
+  }
 
   handleProxy({buffer, next, broadcast}) {
     if (this._proxy === null) {
@@ -55,6 +82,16 @@ export default class ProxyPreset extends IPreset {
   }
 
   clientOut(props) {
+    if (this._mode === MODE_TUNNEL && !this._isTunnelReady) {
+      const {buffer, broadcast, next} = props;
+      broadcast({
+        type: SOCKET_CONNECT_TO_DST,
+        payload: {
+          targetAddress: {type: null, host: this._host, port: this._port},
+          onConnected: () => next(buffer)
+        }
+      });
+    }
     return this.handleProxy(props);
   }
 
