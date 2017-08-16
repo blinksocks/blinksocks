be747f7057ee1f6061d31abe68096c0f43ecb510
Micooz <micooz@hotmail.com>
2017-08-16 13:33:39 +0800

    build: update

--- build/index.js
+++ build/index.js
@@ -1,1 +1,1 @@
-'use strict';module.exports=require('./core');
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});var _core=require('./core');Object.keys(_core).forEach(function(key){if(key==='default'||key==='__esModule')return;Object.defineProperty(exports,key,{enumerable:true,get:function get(){return _core[key]}})});var _presets=require('./presets');Object.keys(_presets).forEach(function(key){if(key==='default'||key==='__esModule')return;Object.defineProperty(exports,key,{enumerable:true,get:function get(){return _presets[key]}})});
