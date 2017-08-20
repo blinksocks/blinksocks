1c880b632f40a0b1642e938ce690dc204b9ac2db
Micooz <micooz@hotmail.com>
2017-08-20 22:08:18 +0800

    build: update

---
+++ build/utils/logger.js
@@ -0,0 +1,1 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});exports.logger=undefined;var _winston=require('winston');var _winston2=_interopRequireDefault(_winston);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}const logger=exports.logger=new _winston2.default.Logger;
