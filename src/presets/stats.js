2a3e645f1401c264f2adcbf14ad39ab6a5589ac7
Micooz <micooz@hotmail.com>
2017-08-31 15:57:57 +0800

    presets: refactor stats.js using IPresetStatic

--- src/presets/stats.js
+++ src/presets/stats.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
-import {IPreset, PRESET_FAILED, CONNECTION_CLOSED, CONNECTION_CREATED} from './defs';
+import path from 'path';
+import {IPresetStatic, PRESET_FAILED, CONNECTION_CLOSED, CONNECTION_CREATED} from './defs';
 
 const now = () => (new Date()).getTime();
 
@@ -11,127 +12,118 @@ const DEFAULT_SAVE_INTERVAL = 60;
  *   Perform statistics among traffic via this preset
  *
  * @params
- *   interval: The sample interval in seconds.
+ *   save_to: Where to store stats result.
+ *   sample_interval(optional): The sample interval in seconds.
+ *   save_interval(optional): The save interval in seconds.
  *
  * @examples
  *   {
  *     "name": "stats",
  *     "params": {
  *       "save_to": "stats.json",
- *       "sample_interval": 1,
- *       "save_interval": 10
+ *       "sample_interval": 30,
+ *       "save_interval": 60
  *     }
  *   }
  */
-export default class StatsPreset extends IPreset {
+export default class StatsPreset extends IPresetStatic {
 
   sampleInterval = DEFAULT_SAMPLE_INTERVAL; // seconds
 
   saveInterval = DEFAULT_SAVE_INTERVAL; // seconds
 
-  static saveTo = '';
+  saveTo = '';
 
-  static sampleTimer = null;
+  sampleTimer = null;
 
-  static saveTimer = null;
-
-  static isHookExit = false;
+  saveTimer = null;
 
   // statistics
 
-  static startedAt = now();
+  startedAt = now();
 
-  static totalOutBytes = 0;
+  totalOutBytes = 0;
 
-  static totalInBytes = 0;
+  totalInBytes = 0;
 
-  static totalOutPackets = 0;
+  totalOutPackets = 0;
 
-  static totalInPackets = 0;
+  totalInPackets = 0;
 
-  static totalErrors = 0;
+  totalErrors = 0;
 
-  static totalConnections = 0;
+  totalConnections = 0;
 
   // calculated
 
-  static instantOutSpeed = 0;
+  instantOutSpeed = 0;
 
-  static instantInSpeed = 0;
+  instantInSpeed = 0;
 
-  static maxOutSpeed = 0;
+  maxOutSpeed = 0;
 
-  static maxInSpeed = 0;
+  maxInSpeed = 0;
 
-  static maxConnections = 0;
+  maxConnections = 0;
 
   // temporary
 
-  static tmpOut = 0;
+  tmpOut = 0;
 
-  static tmpIn = 0;
+  tmpIn = 0;
 
-  constructor(params) {
-    super();
-    const props = {
-      sample_interval: DEFAULT_SAMPLE_INTERVAL,
-      save_interval: DEFAULT_SAVE_INTERVAL,
-      ...params
-    };
+  static checkParams({save_to, sample_interval = DEFAULT_SAMPLE_INTERVAL, save_interval = DEFAULT_SAVE_INTERVAL}) {
     // save_to
-    if (typeof props.save_to !== 'string' || props.save_to.length < 1) {
-      throw Error('\'save_to\' must be provided as a non-empty string');
+    if (typeof save_to !== 'string' || save_to === '') {
+      throw Error('\'save_to\' must be a non-empty string');
     }
     // sample_interval
-    if (typeof props.sample_interval === 'undefined') {
+    if (typeof sample_interval === 'undefined') {
       throw Error('\'sample_interval\' must be provided as an integer');
     }
-    if (typeof props.sample_interval !== 'number') {
+    if (typeof sample_interval !== 'number') {
       throw Error('\'sample_interval\' must be a number');
     }
-    if (!Number.isSafeInteger(props.sample_interval)) {
+    if (!Number.isSafeInteger(sample_interval)) {
       throw Error('\'sample_interval\' must be an integer');
     }
-    if (props.sample_interval < 1) {
+    if (sample_interval < 1) {
       throw Error('\'sample_interval\' must be greater than 0');
     }
     // save_interval
-    if (typeof props.save_interval === 'undefined') {
+    if (typeof save_interval === 'undefined') {
       throw Error('\'save_interval\' must be provided as an integer');
     }
-    if (typeof props.save_interval !== 'number') {
+    if (typeof save_interval !== 'number') {
       throw Error('\'save_interval\' must be a number');
     }
-    if (!Number.isSafeInteger(props.save_interval)) {
+    if (!Number.isSafeInteger(save_interval)) {
       throw Error('\'save_interval\' must be an integer');
     }
-    if (props.save_interval < 1) {
-      throw Error('\'save_interval\' must be greater than 0');
-    }
-    StatsPreset.saveTo = props.save_to;
-    this.sampleInterval = props.sample_interval;
-    this.saveInterval = props.save_interval;
-    // timers
-    if (StatsPreset.sampleTimer === null) {
-      StatsPreset.sampleTimer = setInterval(this.sample.bind(this), props.sample_interval * 1e3);
-    }
-    if (StatsPreset.saveTimer === null) {
-      StatsPreset.saveTimer = setInterval(StatsPreset.save, props.save_interval * 1e3);
-    }
-    // exit hook
-    if (!StatsPreset.isHookExit) {
-      process.on('SIGINT', StatsPreset.save);
-      StatsPreset.isHookExit = true;
+    if (save_interval < 5) {
+      throw Error('\'save_interval\' must be greater than 5');
     }
   }
 
-  static save() {
-    const startedAt = StatsPreset.startedAt;
+  constructor({save_to, sample_interval = DEFAULT_SAMPLE_INTERVAL, save_interval = DEFAULT_SAVE_INTERVAL}) {
+    super();
+    this.onSave = this.onSave.bind(this);
+    this.onSample = this.onSample.bind(this);
+    this.saveTo = path.resolve(process.cwd(), save_to);
+    this.sampleInterval = sample_interval;
+    this.saveInterval = save_interval;
+    this.sampleTimer = setInterval(this.onSample, sample_interval * 1e3);
+    this.saveTimer = setInterval(this.onSave, save_interval * 1e3);
+    process.on('SIGINT', this.onSave);
+  }
+
+  onSave() {
+    const startedAt = this.startedAt;
     const endAt = now();
     const durationMilliSec = endAt - startedAt;
     const durationSec = durationMilliSec / 1e3;
-    const totalPackets = StatsPreset.totalInPackets + StatsPreset.totalOutPackets;
-    const totalBytes = StatsPreset.totalInBytes + StatsPreset.totalOutBytes;
+    const totalPackets = this.totalInPackets + this.totalOutPackets;
+    const totalBytes = this.totalInBytes + this.totalOutBytes;
     const json = {
       sample: {
         from: startedAt,
@@ -139,26 +131,26 @@ export default class StatsPreset extends IPreset {
         duration: durationMilliSec
       },
       summary: {
-        maxOutSpeed: StatsPreset.maxOutSpeed,
-        maxInSpeed: StatsPreset.maxInSpeed,
-        maxConnections: StatsPreset.maxConnections,
-        totalOutBytes: StatsPreset.totalOutBytes,
-        totalOutPackets: StatsPreset.totalOutPackets,
-        totalInBytes: StatsPreset.totalInBytes,
-        totalInPackets: StatsPreset.totalInPackets,
+        maxOutSpeed: this.maxOutSpeed,
+        maxInSpeed: this.maxInSpeed,
+        maxConnections: this.maxConnections,
+        totalOutBytes: this.totalOutBytes,
+        totalOutPackets: this.totalOutPackets,
+        totalInBytes: this.totalInBytes,
+        totalInPackets: this.totalInPackets,
         totalBytes: totalBytes,
         totalPackets: totalPackets,
-        totalErrors: StatsPreset.totalErrors
+        totalErrors: this.totalErrors
       },
       instant: {
-        outSpeed: StatsPreset.instantOutSpeed,
-        inSpeed: StatsPreset.instantInSpeed,
-        totalConnections: StatsPreset.totalConnections,
-        errorRate: totalPackets.length > 0 ? StatsPreset.totalErrors / totalPackets : 0,
-        outBytesPerSecond: StatsPreset.totalOutBytes / durationSec,
-        outPacketsPerSecond: StatsPreset.totalOutPackets / durationSec,
-        inBytesPerSecond: StatsPreset.totalInBytes / durationSec,
-        inPacketsPerSecond: StatsPreset.totalInPackets / durationSec,
+        outSpeed: this.instantOutSpeed,
+        inSpeed: this.instantInSpeed,
+        totalConnections: this.totalConnections,
+        errorRate: totalPackets.length > 0 ? this.totalErrors / totalPackets : 0,
+        outBytesPerSecond: this.totalOutBytes / durationSec,
+        outPacketsPerSecond: this.totalOutPackets / durationSec,
+        inBytesPerSecond: this.totalInBytes / durationSec,
+        inPacketsPerSecond: this.totalInPackets / durationSec,
         totalBytesPerSecond: totalBytes / durationSec,
         totalPacketsPerSecond: totalPackets / durationSec
       },
@@ -168,49 +160,49 @@ export default class StatsPreset extends IPreset {
         memoryUsage: process.memoryUsage()
       }
     };
-    fs.writeFileSync(StatsPreset.saveTo, JSON.stringify(json, null, '  '));
+    fs.writeFileSync(this.saveTo, JSON.stringify(json, null, '  '));
   }
 
-  sample() {
-    StatsPreset.instantOutSpeed = StatsPreset.tmpOut / this.sampleInterval;
-    StatsPreset.instantInSpeed = StatsPreset.tmpIn / this.sampleInterval;
+  onSample() {
+    this.instantOutSpeed = this.tmpOut / this.sampleInterval;
+    this.instantInSpeed = this.tmpIn / this.sampleInterval;
 
-    StatsPreset.maxOutSpeed = Math.max(StatsPreset.maxOutSpeed, StatsPreset.instantOutSpeed);
-    StatsPreset.maxInSpeed = Math.max(StatsPreset.maxInSpeed, StatsPreset.instantInSpeed);
+    this.maxOutSpeed = Math.max(this.maxOutSpeed, this.instantOutSpeed);
+    this.maxInSpeed = Math.max(this.maxInSpeed, this.instantInSpeed);
 
-    StatsPreset.tmpOut = 0;
-    StatsPreset.tmpIn = 0;
+    this.tmpOut = 0;
+    this.tmpIn = 0;
   }
 
   onNotified(action) {
     if (action.type === PRESET_FAILED) {
-      StatsPreset.totalErrors += 1;
+      this.totalErrors += 1;
     }
     if (action.type === CONNECTION_CREATED) {
-      StatsPreset.totalConnections += 1;
-      StatsPreset.maxConnections = Math.max(StatsPreset.maxConnections, StatsPreset.totalConnections);
+      this.totalConnections += 1;
+      this.maxConnections = Math.max(this.maxConnections, this.totalConnections);
     }
     if (action.type === CONNECTION_CLOSED) {
-      StatsPreset.totalConnections -= 1;
+      this.totalConnections -= 1;
     }
   }
 
   onDestroy() {
-    clearInterval(StatsPreset.sampleTimer);
-    clearInterval(StatsPreset.saveTimer);
+    clearInterval(this.sampleTimer);
+    clearInterval(this.saveTimer);
   }
 
   beforeOut({buffer}) {
-    StatsPreset.totalOutBytes += buffer.length;
-    StatsPreset.totalOutPackets += 1;
-    StatsPreset.tmpOut += buffer.length;
+    this.totalOutBytes += buffer.length;
+    this.totalOutPackets += 1;
+    this.tmpOut += buffer.length;
     return buffer;
   }
 
   beforeIn({buffer}) {
-    StatsPreset.totalInBytes += buffer.length;
-    StatsPreset.totalInPackets += 1;
-    StatsPreset.tmpIn += buffer.length;
+    this.totalInBytes += buffer.length;
+    this.totalInPackets += 1;
+    this.tmpIn += buffer.length;
     return buffer;
   }
 
