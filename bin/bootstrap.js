947a260285494886c6951ee7b61b30c428fd27b4
Micooz <micooz@hotmail.com>
2017-09-05 14:33:42 +0800

    bin: remove .js support

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -1,28 +1,16 @@
 const cluster = require('cluster');
 const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
 
 /**
- * get raw config object from js or json
+ * get raw config object from json
  * @param file
  * @returns {object}
  */
 function obtainConfig(file) {
   let json;
   try {
-    const ext = path.extname(file);
-    // TODO: remove .js support in v2.6.x for security reason
-    if (ext === '.js') {
-      // require .js directly
-      delete require.cache[require.resolve(file)];
-      json = require(file);
-      console.warn(chalk.yellow.underline('\n>> WARN: using .js configuration will be deprecated in v2.6.x, please use .json instead. <<\n'));
-    } else {
-      // others are treated as .json
-      const jsonFile = fs.readFileSync(file);
-      json = JSON.parse(jsonFile);
-    }
+    const jsonFile = fs.readFileSync(file);
+    json = JSON.parse(jsonFile);
   } catch (err) {
     throw Error(`fail to load/parse your '${file}': ${err.message}`);
   }
