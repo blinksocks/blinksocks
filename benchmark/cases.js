48668fe721dafc18911f9140b102a141c1138d2e
Micooz <micooz@hotmail.com>
2017-09-14 17:25:02 +0800

    benchmark: add v2ray-vmess benchmark and group test cases

--- benchmark/cases.js
+++ benchmark/cases.js
@@ -1,19 +1,17 @@
-module.exports = [
-  // +-------------------------+
-  // |      SINGLE PRESET      |
-  // +-------------------------+
-
+const single_presets = [
   // ss-base
   [{"name": "ss-base"}],
   // exp-base-with-padding
   [{"name": "exp-base-with-padding", "params": {"salt": "any string"}}],
   // exp-base-auth-stream
-  [{"name": "exp-base-auth-stream", "params": {"method": "aes-256-ctr"}}],
+  [{"name": "exp-base-auth-stream", "params": {"method": "aes-256-ctr"}}]
+];
 
-  // +-------------------------+
-  // |       TWO PRESETS       |
-  // +-------------------------+
+const v2ray_presets = [
+  [{"name": "v2ray-vmess", "params": {"id": "a3482e88-686a-4a58-8126-99c9df64b7bf", "security": "aes-128-gcm"}}]
+];
 
+const shadowsocks_presets = [
   // [shadowsocks stream ciphers]
   [
     {"name": "ss-base"},
@@ -30,12 +28,15 @@ module.exports = [
   // [shadowsocks aead ciphers]
   [
     {"name": "ss-base"},
-    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm", "info": "ss-subkey"}}
-  ],
+    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm"}}
+  ]
+];
+
+const two_presets = [
   // ss-base + aead-random-cipher
   [
     {"name": "ss-base"},
-    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm", "info": "bs-subkey", "factor": 2}}
+    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm"}}
   ],
   // ss-base + obfs-tls1.2-ticket
   [
@@ -55,12 +56,12 @@ module.exports = [
   // exp-base-with-padding + ss-aead-cipher
   [
     {"name": "exp-base-with-padding", "params": {"salt": "any string"}},
-    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm", "info": "ss-subkey"}}
+    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm"}}
   ],
   // exp-base-with-padding + aead-random-cipher
   [
     {"name": "exp-base-with-padding", "params": {"salt": "any string"}},
-    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm", "info": "bs-subkey", "factor": 2}}
+    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm"}}
   ],
   // exp-base-with-padding + obfs-tls1.2-ticket
   [
@@ -81,12 +82,10 @@ module.exports = [
   [
     {"name": "exp-base-auth-stream", "params": {"method": "aes-256-ctr"}},
     {"name": "obfs-random-padding"}
-  ],
+  ]
+];
 
-  // +-------------------------+
-  // |      THREE PRESETS      |
-  // +-------------------------+
-
+const three_presets = [
   // ss-base + ss-stream-cipher + obfs-tls1.2-ticket
   [
     {"name": "ss-base"},
@@ -102,19 +101,19 @@ module.exports = [
   // ss-base + ss-aead-cipher + obfs-tls1.2-ticket
   [
     {"name": "ss-base"},
-    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm", "info": "ss-subkey"}},
+    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm"}},
     {"name": "obfs-tls1.2-ticket", "params": {"sni": ["test.com"]}}
   ],
   // ss-base + obfs-random-padding + ss-aead-cipher
   [
     {"name": "ss-base"},
     {"name": "obfs-random-padding"},
-    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm", "info": "ss-subkey"}}
+    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm"}}
   ],
   // ss-base + aead-random-cipher + obfs-tls1.2-ticket
   [
     {"name": "ss-base"},
-    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm", "info": "bs-subkey", "factor": 2}},
+    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm"}},
     {"name": "obfs-tls1.2-ticket", "params": {"sni": ["test.com"]}}
   ],
   // exp-base-with-padding + ss-stream-cipher + obfs-tls1.2-ticket
@@ -132,19 +131,27 @@ module.exports = [
   // exp-base-with-padding + ss-aead-cipher + obfs-tls1.2-ticket
   [
     {"name": "exp-base-with-padding", "params": {"salt": "any string"}},
-    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm", "info": "ss-subkey"}},
+    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm"}},
     {"name": "obfs-tls1.2-ticket", "params": {"sni": ["test.com"]}}
   ],
   // exp-base-with-padding + obfs-random-padding + ss-aead-cipher
   [
     {"name": "exp-base-with-padding", "params": {"salt": "any string"}},
     {"name": "obfs-random-padding"},
-    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm", "info": "ss-subkey"}}
+    {"name": "ss-aead-cipher", "params": {"method": "aes-256-gcm"}}
   ],
   // exp-base-with-padding + aead-random-cipher + obfs-tls1.2-ticket
   [
     {"name": "exp-base-with-padding", "params": {"salt": "any string"}},
-    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm", "info": "bs-subkey", "factor": 2}},
+    {"name": "aead-random-cipher", "params": {"method": "aes-256-gcm"}},
     {"name": "obfs-tls1.2-ticket", "params": {"sni": ["test.com"]}}
-  ],
+  ]
+];
+
+module.exports = [
+  ...single_presets,
+  ...v2ray_presets,
+  ...shadowsocks_presets,
+  ...two_presets,
+  ...three_presets
 ];
