6d93761f09cae20381423d96b20d9b06663f47fd
Micooz <micooz@hotmail.com>
2017-09-19 14:05:49 +0800

    presets/tracker: extract dump() method to global static function

--- src/presets/tracker.js
+++ src/presets/tracker.js
@@ -6,6 +6,42 @@ const TRACK_CHAR_DOWNLOAD = 'd';
 const TRACK_MAX_SIZE = 40;
 
 /**
+ * print connection track string, and only display the
+ * leading and the trailing TRACK_MAX_SIZE / 2
+ */
+function dump(tracks) {
+  let strs = [];
+  let dp = 0, db = 0;
+  let up = 0, ub = 0;
+  let ud = '';
+  for (const el of tracks) {
+    if (el === TRACK_CHAR_UPLOAD || el === TRACK_CHAR_DOWNLOAD) {
+      if (ud === el) {
+        continue;
+      }
+      ud = el;
+    }
+    if (Number.isInteger(el)) {
+      if (ud === TRACK_CHAR_DOWNLOAD) {
+        dp += 1;
+        db += el;
+      }
+      if (ud === TRACK_CHAR_UPLOAD) {
+        up += 1;
+        ub += el;
+      }
+    }
+    strs.push(el);
+  }
+  const perSize = Math.floor(TRACK_MAX_SIZE / 2);
+  if (strs.length > TRACK_MAX_SIZE) {
+    strs = strs.slice(0, perSize).concat([' ... ']).concat(strs.slice(-perSize));
+  }
+  const summary = __IS_CLIENT__ ? `out/in = ${up}/${dp}, ${ub}b/${db}b` : `in/out = ${dp}/${up}, ${db}b/${ub}b`;
+  logger.info(`[tracker] summary(${summary}) abstract(${strs.join(' ')})`);
+}
+
+/**
  * @description
  *   Track data send/receive events via this preset, and print a part of them after connection closed.
  *
@@ -31,12 +67,18 @@ export default class TrackerPreset extends IPreset {
         this._tracks.push(`${host}:${port}`);
         break;
       case CONNECTION_CLOSED:
-        this.dump();
-        this._tracks = [];
+        dump(this._tracks);
         break;
       default:
         break;
     }
+  }
+
+  onDestroy() {
+    if (this._tracks !== null) {
+      dump(this._tracks);
+    }
+    this._tracks = null;
   }
 
   beforeOut({buffer}) {
@@ -49,42 +91,6 @@ export default class TrackerPreset extends IPreset {
     this._tracks.push(TRACK_CHAR_DOWNLOAD);
     this._tracks.push(buffer.length);
     return buffer;
-  }
-
-  /**
-   * print connection track string, and only display the
-   * leading and the trailing TRACK_MAX_SIZE / 2
-   */
-  dump() {
-    let strs = [];
-    let dp = 0, db = 0;
-    let up = 0, ub = 0;
-    let ud = '';
-    for (const el of this._tracks) {
-      if (el === TRACK_CHAR_UPLOAD || el === TRACK_CHAR_DOWNLOAD) {
-        if (ud === el) {
-          continue;
-        }
-        ud = el;
-      }
-      if (Number.isInteger(el)) {
-        if (ud === TRACK_CHAR_DOWNLOAD) {
-          dp += 1;
-          db += el;
-        }
-        if (ud === TRACK_CHAR_UPLOAD) {
-          up += 1;
-          ub += el;
-        }
-      }
-      strs.push(el);
-    }
-    const perSize = Math.floor(TRACK_MAX_SIZE / 2);
-    if (strs.length > TRACK_MAX_SIZE) {
-      strs = strs.slice(0, perSize).concat([' ... ']).concat(strs.slice(-perSize));
-    }
-    const summary = __IS_CLIENT__ ? `out/in = ${up}/${dp}, ${ub}b/${db}b` : `in/out = ${dp}/${up}, ${db}b/${ub}b`;
-    logger.info(`[tracker] summary(${summary}) abstract(${strs.join(' ')})`);
   }
 
 }
