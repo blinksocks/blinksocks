cd15044aa4976850f85854d91a96e4b0503240d8
Micooz <micooz@hotmail.com>
2017-10-18 13:54:31 +0800

    core: disable json output to log files

--- src/core/config.js
+++ src/core/config.js
@@ -86,6 +86,8 @@ export class Config {
           prettyPrint: true
         }),
         new (require('winston-daily-rotate-file'))({
+          json: false,
+          eol: os.EOL,
           filename: __LOG_PATH__,
           level: __LOG_LEVEL__,
           maxDays: __LOG_MAX_DAYS__
