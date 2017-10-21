5aa1cfc755844cbfa5a352434b241091c23dc326
Micooz <micooz@hotmail.com>
2017-10-21 11:59:00 +0800

    src: refine api interfaces

--- src/transports/index.js
+++ src/transports/index.js
@@ -1,19 +1,3 @@
-import uniqueId from 'lodash.uniqueid';
-import {Relay} from '../core';
-import {TcpInbound, TcpOutbound} from './tcp';
-import {TlsInbound, TlsOutbound} from './tls';
-import {WsInbound, WsOutbound} from './websocket';
-
-const mapping = {
-  'tcp': [TcpInbound, TcpOutbound],
-  'tls': [TlsInbound, TlsOutbound],
-  'ws': [WsInbound, WsOutbound]
-};
-
-export function createRelay(transport, context, proxyRequest = null) {
-  const [Inbound, Outbound] = __IS_CLIENT__ ? [TcpInbound, mapping[transport][1]] : [mapping[transport][0], TcpOutbound];
-  const props = {context, Inbound, Outbound, proxyRequest};
-  const relay = new Relay(props);
-  relay.id = uniqueId(`${transport}_`);
-  return relay;
-}
+export * from './tcp';
+export * from './tls';
+export * from './websocket';
