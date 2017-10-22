1266316210aaaf9840a8d685c94e8bed03efa0bc
Micooz <micooz@hotmail.com>
2017-10-22 11:42:08 +0800

    package: fix pkg
    Signed-off-by: Micooz <micooz@hotmail.com>

--- bin/modules.js
+++ bin/modules.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'development') {
-  require('babel-register');
   // https://github.com/zeit/pkg/issues/261
+  require('babel-register', 'must-exclude');
   module.exports = require('../src', 'must-exclude');
 } else {
   module.exports = require('../lib');
