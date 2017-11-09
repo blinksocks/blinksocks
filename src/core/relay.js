17399e4c0d05a076b4c11ddc93c50653a8c9394a
Micooz <micooz@hotmail.com>
2017-11-09 11:03:43 +0800

    core: support dynamic change preset suite

--- src/core/relay.js
+++ src/core/relay.js
@@ -2,7 +2,7 @@ import EventEmitter from 'events';
 import uniqueId from 'lodash.uniqueid';
 import {Pipe} from './pipe';
 import {PIPE_ENCODE, PIPE_DECODE} from './middleware';
-import {CONNECT_TO_REMOTE, CONNECTION_CREATED} from '../presets';
+import {CONNECT_TO_REMOTE, CONNECTION_CREATED, CHANGE_PRESET_SUITE} from '../presets';
 import {
   TcpInbound, TcpOutbound,
   UdpInbound, UdpOutbound,
@@ -10,11 +10,16 @@ import {
   WsInbound, WsOutbound
 } from '../transports';
 
-function preparePresets() {
-  let presets = __PRESETS__;
-  // add "tracker" preset to the preset list on both sides
-  if (presets[presets.length - 1].name !== 'tracker') {
-    presets = presets.concat([{'name': 'tracker'}]);
+function preparePresets(presets) {
+  const auto_conf_preset = presets.find(({name}) => name === 'auto-conf');
+  if (auto_conf_preset !== undefined) {
+    presets = [auto_conf_preset]; // drop any other presets when have "auto-conf" preset
+  } else {
+    // add "tracker" preset to the preset list on both sides
+    const last = presets[presets.length - 1];
+    if (!last || last.name !== 'tracker') {
+      presets = presets.concat([{'name': 'tracker'}]);
+    }
   }
   return presets;
 }
@@ -22,11 +27,15 @@ function preparePresets() {
 // .on('close')
 export class Relay extends EventEmitter {
 
+  _transport = null;
+
+  _context = null;
+
+  _proxyRequest = null;
+
   _inbound = null;
 
   _outbound = null;
-
-  _transport = null;
 
   _pipe = null;
 
@@ -36,11 +45,13 @@ export class Relay extends EventEmitter {
     super();
     this.setPresets = this.setPresets.bind(this);
     this.onBroadcast = this.onBroadcast.bind(this);
-    this.postPipeForward = this.postPipeForward.bind(this);
-    this.postPipeBackward = this.postPipeBackward.bind(this);
+    this.postPipeEncode = this.postPipeEncode.bind(this);
+    this.postPipeDecode = this.postPipeDecode.bind(this);
     this._transport = transport;
+    this._context = context;
+    this._proxyRequest = proxyRequest;
     // pipe
-    this._presets = preparePresets();
+    this._presets = preparePresets(__PRESETS__);
     this._pipe = this.createPipe(this._presets);
     // outbound
     this._inbound = new Inbound({context: context, pipe: this._pipe});
@@ -73,11 +84,53 @@ export class Relay extends EventEmitter {
   // hooks of pipe
 
   onBroadcast(action) {
-    this._inbound && this._inbound.onBroadcast(action);
-    this._outbound && this._outbound.onBroadcast(action);
+    switch (action.type) {
+      case CHANGE_PRESET_SUITE:
+        this.onChangePresetSuite(action);
+        break;
+      default:
+        this._inbound && this._inbound.onBroadcast(action);
+        this._outbound && this._outbound.onBroadcast(action);
+        break;
+    }
   }
 
-  postPipeForward(buffer) {
+  onChangePresetSuite(action) {
+    const {type, presets, data, createWrapper} = action.payload;
+    // 1. replace preset list
+    this.setPresets(preparePresets(presets));
+    // 2. handle post-pipe event when encode
+    if (__IS_CLIENT__ && type === PIPE_ENCODE) {
+      const event = `post_${PIPE_ENCODE}`;
+      this._pipe.removeListener(event, this.postPipeEncode);
+      this._pipe.once(event, (buffer) => {
+        this.postPipeEncode(createWrapper(buffer));
+        this._pipe.on(event, this.postPipeEncode);
+      });
+    }
+    // 3. initialize newly created presets
+    const transport = this._transport;
+    const context = this._context;
+    const proxyRequest = this._proxyRequest;
+    this._pipe.broadcast('pipe', {
+      type: CONNECTION_CREATED,
+      payload: {
+        transport: transport,
+        host: context.remoteAddress,
+        port: context.remotePort
+      }
+    });
+    if (__IS_CLIENT__ && proxyRequest !== null) {
+      this._pipe.broadcast(null, {
+        type: CONNECT_TO_REMOTE,
+        payload: {...proxyRequest, keepAlive: true} // keep previous connection alive, don't re-connect
+      });
+    }
+    // 4. re-pipe
+    this._pipe.feed(type, data);
+  }
+
+  postPipeEncode(buffer) {
     if (__IS_CLIENT__) {
       this._outbound.write(buffer);
     } else {
@@ -85,7 +138,7 @@ export class Relay extends EventEmitter {
     }
   }
 
-  postPipeBackward(buffer) {
+  postPipeDecode(buffer) {
     if (__IS_CLIENT__) {
       this._inbound.write(buffer);
     } else {
@@ -97,10 +150,10 @@ export class Relay extends EventEmitter {
 
   /**
    * set a new presets and recreate the pipe
-   * @param callback
+   * @param value
    */
-  setPresets(callback) {
-    this._presets = callback(this._presets);
+  setPresets(value) {
+    this._presets = typeof value === 'function' ? value(this._presets) : value;
     this._pipe.destroy();
     this._pipe = this.createPipe(this._presets);
     this._inbound._pipe = this._pipe;
@@ -113,8 +166,8 @@ export class Relay extends EventEmitter {
   createPipe(presets) {
     const pipe = new Pipe({presets, isUdp: this._transport === 'udp'});
     pipe.on('broadcast', this.onBroadcast.bind(this)); // if no action were caught by presets
-    pipe.on(`post_${PIPE_ENCODE}`, this.postPipeForward);
-    pipe.on(`post_${PIPE_DECODE}`, this.postPipeBackward);
+    pipe.on(`post_${PIPE_ENCODE}`, this.postPipeEncode);
+    pipe.on(`post_${PIPE_DECODE}`, this.postPipeDecode);
     return pipe;
   }
 
@@ -129,6 +182,8 @@ export class Relay extends EventEmitter {
     this._inbound = null;
     this._outbound = null;
     this._presets = null;
+    this._context = null;
+    this._proxyRequest = null;
   }
 
 }
