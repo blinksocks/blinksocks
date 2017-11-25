d91c725fbd1077f51ca3cd23072d0744ec06c53c
Micooz <micooz@hotmail.com>
2017-11-25 12:44:00 +0800

    presets/auto-conf: protocol changed

--- src/presets/auto-conf.js
+++ src/presets/auto-conf.js
@@ -17,6 +17,7 @@ import {PIPE_DECODE, PIPE_ENCODE} from '../core/middleware';
 
 const readFile = promisify(fs.readFile);
 const MAX_TIME_DIFF = 30; // seconds
+const NOOP = Buffer.alloc(0);
 
 /**
  * @description
@@ -44,6 +45,7 @@ const MAX_TIME_DIFF = 30; // seconds
  *   +------------+-----------+------------+-------------+
  *   |     2      |     4     |     16     |  Variable   |
  *   +------------+-----------+------------+-------------+
+ *                |<-- RC4 -->|
  *
  *   # TCP chunks (client <-> server)
  *   +-------------+
@@ -60,10 +62,11 @@ const MAX_TIME_DIFF = 30; // seconds
  *   +-------------+
  *
  * @explain
- *   1. Suite ID (0 ~ 0xFFFF) is randomly generated and mapped to real one (Suite ID % suites.length) in the pre-shared suites.
- *   2. Suite ID, UTC are little-endian.
- *   3. HMAC-MD5 is HMAC(Suite ID + UTC).
- *   4. HMAC-MD5 key is EVP_BytesToKey(base64(orgKey) + base64(md5(Suite ID)), 16, 16).
+ *   1. Suite ID should be randomly generated and mapped to real one (Suite ID % suites.length) in the pre-shared suites.
+ *   2. Suite ID and UTC are little-endian.
+ *   3. UTC is encrypted by RC4.
+ *   4. HMAC-MD5 is HMAC(Suite ID + RC4(UTC)).
+ *   5. RC4 and HMAC-MD5 key are EVP_BytesToKey(base64(orgKey) + base64(md5(Suite ID)), 16, 16).
  */
 export default class AutoConfPreset extends IPreset {
 
@@ -108,12 +111,13 @@ export default class AutoConfPreset extends IPreset {
   createRequestHeader(suites) {
     const sid = crypto.randomBytes(2);
     const utc = ntb(getCurrentTimestampInt(), 4, BYTE_ORDER_LE);
-    const hmac_key = EVP_BytesToKey(Buffer.from(__KEY__).toString('base64') + hash('md5', sid).toString('base64'), 16, 16);
-    const request_hmac = hmac('md5', hmac_key, Buffer.concat([sid, utc]));
-    const suite = suites[sid.readUInt16LE(0) % suites.length];
+    const key = EVP_BytesToKey(Buffer.from(__KEY__).toString('base64') + hash('md5', sid).toString('base64'), 16, 16);
+    const cipher = crypto.createCipheriv('rc4', key, NOOP);
+    const enc_utc = cipher.update(utc);
+    const request_hmac = hmac('md5', key, Buffer.concat([sid, enc_utc]));
     return {
-      header: Buffer.concat([sid, utc, request_hmac]),
-      suite: suite
+      header: Buffer.concat([sid, enc_utc, request_hmac]),
+      suite: suites[sid.readUInt16LE(0) % suites.length]
     };
   }
 
@@ -145,12 +149,14 @@ export default class AutoConfPreset extends IPreset {
     }
     const sid = buffer.slice(0, 2);
     const request_hmac = buffer.slice(6, 22);
-    const hmac_key = EVP_BytesToKey(Buffer.from(__KEY__).toString('base64') + hash('md5', sid).toString('base64'), 16, 16);
-    const hmac_calc = hmac('md5', hmac_key, buffer.slice(0, 6));
+    const key = EVP_BytesToKey(Buffer.from(__KEY__).toString('base64') + hash('md5', sid).toString('base64'), 16, 16);
+    const hmac_calc = hmac('md5', key, buffer.slice(0, 6));
     if (!hmac_calc.equals(request_hmac)) {
       return fail(`unexpected hmac of client request, dump=${dumpHex(buffer)}`);
     }
-    const utc = buffer.readUInt32LE(2);
+    const enc_utc = buffer.slice(2, 6);
+    const decipher = crypto.createDecipheriv('rc4', key, NOOP);
+    const utc = decipher.update(enc_utc).readUInt32LE(0);
     const time_diff = Math.abs(utc - getCurrentTimestampInt());
     if (time_diff > MAX_TIME_DIFF) {
       return fail(`timestamp diff is over ${MAX_TIME_DIFF}s, dump=${dumpHex(buffer)}`);
