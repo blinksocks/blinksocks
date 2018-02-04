6d2ae7ef15aad2c6f0f8ce115f6e06b991afb251
Micooz <micooz@hotmail.com>
2018-02-04 22:34:31 +0800

    src/constants: set APP_ID randomly when application startsup

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
+import {randomBytes} from './utils/crypto';
+
+export const APP_ID = randomBytes(16).toString('hex');
 export const PIPE_ENCODE = 1;
 export const PIPE_DECODE = -1;
 export const MAX_BUFFERED_SIZE = 512 * 1024; // 512KB
